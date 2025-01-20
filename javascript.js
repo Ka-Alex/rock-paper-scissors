0464a982bea9fa5e01e710218578420820892da1
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-01-20 16:45:54 +0100

    Add the algorithm for the computer choice
    Write the logic in comments to get then computer choice before coding phase

---
+++ javascript.js
@@ -0,0 +1,5 @@
+//VARIABLE: create an integer variable for computerChoice, initVal = 0
+//FUNCTION: Randomly chooses a number between 0 and 2
+    //CONDITION: 0 for Rock, 1 for Scissors, 2 for Paper
+//AFFECTATION: put this value inside humanChoice   
+
