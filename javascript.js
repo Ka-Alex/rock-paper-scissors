c12b7d5f0d9c699c02fc51826f3e30872377e7a8
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-01-20 16:55:25 +0100

    Add the code to get the computer choice
    Add a function that randomly returns "rock", "paper" or "scissors"

--- javascript.js
+++ javascript.js
@@ -1,5 +1,26 @@
 //VARIABLE: create an integer variable for computerChoice, initVal = 0
-//FUNCTION: Randomly chooses a number between 0 and 2
-    //CONDITION: 0 for Rock, 1 for Scissors, 2 for Paper
-//AFFECTATION: put this value inside humanChoice   
+let computerChoice = 0;
 
+//FUNCTION: Randomly chooses a number between 0 and 2            
+function getComputerChoice() {
+    //returns 0, 1 or 2
+    computerChoice = Math.floor(Math.random()*3);
+    switch (computerChoice){
+        case 0: 
+            computerChoice = "Rock";
+            break;
+        case 1: 
+            computerChoice = "Scissors";
+            break;
+        case 2: 
+            computerChoice = "Paper";
+            break;
+        default : console.log("Invalid choice");
+    }
+
+    return computerChoice;
+}
+
+//AFFECTATION: put this string value inside humanChoice
+computerChoice = getComputerChoice();
+console.log(computerChoice);
