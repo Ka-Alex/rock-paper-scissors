0f1b91f2570b1a8d9e1ed1f2d48ee141159d525a
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-02-07 15:04:30 +0100

    Add ui to the project
    The player is able to play the game by clicking on buttons

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let humanChoice = "";
 let humanScore = 0;
 let computerScore = 0;
 
+
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
@@ -22,15 +23,10 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getHumanChoice() {
-    humanChoice = prompt("Rock, Scissors or Paper ?");
-    // user cancel the prompt window
-    if (humanChoice === null) {
-        return;
-    } else {
-        humanChoice = humanChoice.toLowerCase()
-    }
+function getHumanChoice(event) {
 
+    humanChoice = event.target.id.toLocaleLowerCase();
+  
     // allow only the three string values
     if ((humanChoice === "rock") || (humanChoice === "scissors") || (humanChoice === "paper")) {
         return humanChoice;
@@ -40,72 +36,71 @@ function getHumanChoice() {
     }
 }
 
-// score round by round and final score with finalRound parameter
-function countScore(humanScore, computerScore, finalRound) {
-    let winnerOrLoser = "";
-    if(humanScore > computerScore) {
-        winnerOrLoser = `You win the game ${`\u{1F973}`}!`
-    } else if(humanScore < computerScore) {
-        winnerOrLoser = `You lose the game ${`\u{1F62D}`}!`
-    } else {
-        winnerOrLoser = `It's a tie ${`\u{1F635}`}!`
-    }
-    if(finalRound){
-        console.log(`%c Final results : ${winnerOrLoser}`,
-            `font-size: 20px; color: red`);
-            console.table({"You":`${humanScore} point(s)`, "Computer":`${computerScore} point(s)`})
-    }
-}
-
-// a game is 5 rounds
-function playGame(round = 0) {
-
-    console.log(`%c Start of the game`, `font-size: 20px; color: blue`);
-    
-    function playRound() {
-        ++round;
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+function playRound(event) {
+    const humanSelection = getHumanChoice(event);
+    const computerSelection = getComputerChoice();
 
             switch (humanSelection + computerSelection) {
                 case "rockscissors":
                 case "scissorspaper":
                 case "paperrock":
-                    console.log(`%c Round ${round}: `,`font-size: 15px`, `You win the round ${"\u{1F60E}"}! ${humanSelection} beats ${computerSelection}`);
+                    console.log(`You win ${"\u{1F60E}"}! ${humanSelection} beats ${computerSelection}`);
                     humanScore++; 
                     break;
                 case "rockpaper":
                 case "paperscissors":
                 case "scissorsrock":  
-                    console.log(`%c Round ${round}: `,`font-size: 15px`,`You lose the round ${"\u{1F62D}"}! ${humanSelection} lose to ${computerSelection}`);
+                    console.log(`You lose ${"\u{1F62D}"}! ${humanSelection} lose to ${computerSelection}`);
                     computerScore++;
                     break;
                 case "rockrock":
                 case "scissorsscissors":
                 case "paperpaper":        
-                    console.log(`%c Round ${round}: `,`font-size: 15px`, `It's a tie in this round ${"\u{1F632}"}! You both chose ${humanSelection}`);
+                    console.log(`It's a tie ${"\u{1F632}"}! You both chose ${humanSelection}`);
                     break;    
             }
+           //++round;
            }
 
-    playRound();
-    countScore(humanScore, computerScore)
 
-    playRound();
-    countScore(humanScore, computerScore)
 
-    playRound();
-    countScore(humanScore, computerScore)
-
-    playRound();
-   countScore(humanScore, computerScore)
-
-    playRound();
-    countScore(humanScore, computerScore)
+// score round by round and final score with finalRound parameter
+function countScore(humanScore, computerScore, finalRound) {
+    // let winnerOrLoser = "";
+    // if(humanScore > computerScore) {
+    //     winnerOrLoser = `You win the game ${`\u{1F973}`}!`
+    // } else if(humanScore < computerScore) {
+    //     winnerOrLoser = `You lose the game ${`\u{1F62D}`}!`
+    // } else {
+    //     winnerOrLoser = `It's a tie ${`\u{1F635}`}!`
+    // }
+    // if(finalRound){
+    //     console.log(`%c Final results : ${winnerOrLoser}`,
+    //         `font-size: 20px; color: red`);
+    //         console.table({"You":`${humanScore} point(s)`, "Computer":`${computerScore} point(s)`})
+    // }
 }
 
-playGame();
+// a game is 5 rounds
+function playGame() {
+//     console.log(`%c Start of the game`, `font-size: 20px; color: blue`);
+
+//     playRound();
+//     countScore(humanScore, computerScore)
+
+}
+
+//playGame();
 
 //Final score
-countScore(humanScore, computerScore, true);
+//countScore(humanScore, computerScore, true);
+
+
+    const btnRock = document.querySelector('#rock');
+    const btnPaper = document.querySelector('#paper');
+    const btnScissors = document.querySelector('#scissors');
+
+    btnRock.addEventListener('click', playRound);
+    btnPaper.addEventListener('click', playRound);
+    btnScissors.addEventListener('click', playRound);
+    
