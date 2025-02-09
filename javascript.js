2c3666440127f76094d761855232b9867e7e0f7e
KA <191128298+KarlAlexander@users.noreply.github.com>
2025-02-09 18:06:00 +0100

    Add the logic to play the game with a score system
    Display the running score and announce a winner of the game once one player reaches 5 points

--- javascript.js
+++ javascript.js
@@ -1,9 +1,18 @@
 "use strict"
 let computerChoice = 0;
 let humanChoice = "";
+
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
+const btnRock = document.querySelector('#rock');
+const btnPaper = document.querySelector('#paper');
+const btnScissors = document.querySelector('#scissors');
+
+btnRock.addEventListener('click', playRound);
+btnPaper.addEventListener('click', playRound);
+btnScissors.addEventListener('click', playRound);
 
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3);
@@ -26,7 +35,7 @@ function getComputerChoice() {
 function getHumanChoice(event) {
 
     humanChoice = event.target.id.toLocaleLowerCase();
-  
+
     // allow only the three string values
     if ((humanChoice === "rock") || (humanChoice === "scissors") || (humanChoice === "paper")) {
         return humanChoice;
@@ -37,93 +46,97 @@ function getHumanChoice(event) {
 }
 
 function playRound(event) {
+
+    round++;
+    let isFinalRound = false;
+
     const humanSelection = getHumanChoice(event);
     const computerSelection = getComputerChoice();
 
-            switch (humanSelection + computerSelection) {
-                case "rockscissors":
-                case "scissorspaper":
-                case "paperrock":
-                    displayResults('win', humanSelection, computerSelection)
-                    humanScore++; 
-                    break;
-                case "rockpaper":
-                case "paperscissors":
-                case "scissorsrock": 
-                    displayResults('lose', humanSelection, computerSelection) 
-                    computerScore++;
-                    break;
-                case "rockrock":
-                case "scissorsscissors":
-                case "paperpaper":
-                    displayResults('tie', humanSelection, computerSelection)        
-                    break;    
-            }
-           //++round;
-           }
-        
-    function displayResults(result, humanSelection, computerSelection) {
-        const div = document.createElement('div');
-        document.body.appendChild(div);
-        let resultMessage ='';
-
-        switch(result) {
-            case 'win':
-                resultMessage = document.createTextNode(`You win ${"\u{1F60E}"}! ${humanSelection} beats ${computerSelection}`);
-                break;
-            case 'lose':
-                resultMessage = document.createTextNode(`You lose ${"\u{1F62D}"}! ${humanSelection} lose to ${computerSelection}`);
-                break;
-            case 'tie':
-                resultMessage = document.createTextNode(`It's a tie ${"\u{1F632}"}! You both chose ${humanSelection}`);
-                break;
-
-        }
-        div.appendChild(resultMessage);
-
+    switch (humanSelection + computerSelection) {
+        case "rockscissors":
+        case "scissorspaper":
+        case "paperrock":
+            humanScore++;
+            displayResults(round, 'win', humanSelection, computerSelection);
+            break;
+        case "rockpaper":
+        case "paperscissors":
+        case "scissorsrock":
+            computerScore++;
+            displayResults(round, 'lose', humanSelection, computerSelection);
+            break;
+        case "rockrock":
+        case "scissorsscissors":
+        case "paperpaper":
+            displayResults(round, 'tie', humanSelection, computerSelection);
+            break;
     }
 
-
-
-// score round by round and final score with finalRound parameter
-function countScore(humanScore, computerScore, finalRound) {
-    // let winnerOrLoser = "";
-    // if(humanScore > computerScore) {
-    //     winnerOrLoser = `You win the game ${`\u{1F973}`}!`
-    // } else if(humanScore < computerScore) {
-    //     winnerOrLoser = `You lose the game ${`\u{1F62D}`}!`
-    // } else {
-    //     winnerOrLoser = `It's a tie ${`\u{1F635}`}!`
-    // }
-    // if(finalRound){
-    //     console.log(`%c Final results : ${winnerOrLoser}`,
-    //         `font-size: 20px; color: red`);
-    //         console.table({"You":`${humanScore} point(s)`, "Computer":`${computerScore} point(s)`})
-    // }
+    countScore(round, humanScore, computerScore, isFinalRound);
+    
 }
 
-// a game is 5 rounds
-function playGame() {
-//     console.log(`%c Start of the game`, `font-size: 20px; color: blue`);
+function displayResults(round, result, humanSelection, computerSelection, isFinalRound = false) {
+    const div = document.createElement('div');
+    document.body.appendChild(div);
+    let resultMessage = '';
 
-//     playRound();
-//     countScore(humanScore, computerScore)
+    if(!isFinalRound) {
+        switch (result) {
+            case 'win':
+                resultMessage = document.createTextNode(`Round ${round}: You win ${"\u{1F60E}"}! ${humanSelection} beats ${computerSelection}`);
+                break;
+            case 'lose':
+                resultMessage = document.createTextNode(`Round ${round}:  You lose ${"\u{1F62D}"}! ${humanSelection} lose to ${computerSelection}`);
+                break;
+            case 'tie':
+                resultMessage = document.createTextNode(`Round ${round}:  It's a tie ${"\u{1F632}"}! You both chose ${humanSelection}`);
+                break;
+    
+        }
+        div.style.marginTop = '10px';
+        div.appendChild(resultMessage);
+       
+    } else {
+        switch (result) {
+            case 'win':
+                resultMessage = document.createTextNode(`You win the game ${"\u{1F60E}"}! ${humanScore} - ${computerScore}`);
+                div.style.color = '#097969';
+                break;
+            case 'lose':
+                resultMessage = document.createTextNode(`You lose the game ${"\u{1F62D}"}! ${computerScore} - ${humanScore} `);
+                div.style.color = '#C70039';
+                break;
+            case 'tie':
+                resultMessage = document.createTextNode(`It's a tie ${"\u{1F632}"}!`);
+                break;
+    
+        }
+        div.style.fontSize = '30px';
+        div.appendChild(resultMessage);
+    }
+}
+
+function countScore(round, humanScore, computerScore, isFinalRound) {
+
+    //The first to five points wins the game
+    if (humanScore === 5 || computerScore === 5) {
+        isFinalRound = true;
+
+        btnRock.removeEventListener('click', playRound);
+        btnPaper.removeEventListener('click', playRound);
+        btnScissors.removeEventListener('click', playRound);
+
+        if (humanScore > computerScore) {
+            displayResults(round, 'win',"","", true);
+        } else if (humanScore < computerScore) {
+            displayResults(round, 'lose',"","", true);
+        } else {
+            displayResults(round,'tie',"","", true);
+        }
+    }
 
 }
 
-//playGame();
 
-//Final score
-//countScore(humanScore, computerScore, true);
-
-
-    const btnRock = document.querySelector('#rock');
-    const btnPaper = document.querySelector('#paper');
-    const btnScissors = document.querySelector('#scissors');
-
-    btnRock.addEventListener('click', playRound);
-    btnPaper.addEventListener('click', playRound);
-    btnScissors.addEventListener('click', playRound);
-
-    
-    
